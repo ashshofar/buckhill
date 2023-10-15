bb2119fbba64be4d424c5969ffc5c5ac5fb17468
inu <inu@voxteneo.com>
2023-10-15 14:58:29 +0700

    #000000 product seeder

--- laravel-petshop/app/Domain/Product/Models/Product.php
+++ laravel-petshop/app/Domain/Product/Models/Product.php
@@ -2,11 +2,28 @@
 
 namespace App\Domain\Product\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        //
+        'category_uuid',
+        'title',
+        'price',
+        'description',
+        'metadata',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->uuid = (string) Str::uuid();
+        });
+    }
 }
---
+++ laravel-petshop/database/factories/Domain/Product/Models/ProductFactory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Factories\Domain\Product\Models;
+
+use App\Domain\File\Models\File;
+use App\Domain\Product\Models\Brand;
+use App\Domain\Product\Models\Category;
+use App\Domain\Product\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_uuid' => $this->getCategories()->uuid,
+            'title' => fake()->word,
+            'price' => fake()->numberBetween(100, 500),
+            'description' => fake()->text,
+            'metadata' => json_encode([
+                'brand' => $this->getBrands()->uuid,
+                'image' => $this->getFiles()->uuid
+            ])
+        ];
+    }
+
+    /**
+     * Find random brands
+     *
+     * @return mixed
+     */
+    public function getBrands(): mixed
+    {
+        return Brand::limit(5)->get()->random(1)->first();
+    }
+
+    /**
+     * Find random files
+     *
+     * @return mixed
+     */
+    public function getFiles(): mixed
+    {
+        return File::limit(5)->get()->random(1)->first();
+    }
+
+    /**
+     * Find random category
+     *
+     * @return mixed
+     */
+    public function getCategories(): mixed
+    {
+        return Category::limit(5)->get()->random(1)->first();
+    }
+
+}
