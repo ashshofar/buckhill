366ab28d0b7b502a625659d301a70ead7a254b3d
inu <inu@voxteneo.com>
2023-10-15 14:42:51 +0700

    #000000 create product migration

---
+++ laravel-petshop/database/migrations/2023_10_15_062906_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->index('uuid')->change();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_uuid');
+            $table->uuid();
+            $table->string('title');
+            $table->float('price');
+            $table->text('description');
+            $table->json('metadata');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_uuid')->references('uuid')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropIndex('categories_uuid_index');
+        });
+    }
+};
