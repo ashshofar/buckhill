4549b59f04140c2e2bb8bb1bd524eb0d3411bf7d
inu <inu@voxteneo.com>
2023-10-22 11:30:02 +0700

    #000000 add api doc

--- laravel-petshop/app/Domain/File/Controllers/File/GetFileController.php
+++ laravel-petshop/app/Domain/File/Controllers/File/GetFileController.php
@@ -5,7 +5,27 @@ namespace App\Domain\File\Controllers\File;
 use App\Domain\File\BLL\File\FileBLLInterface;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\ApiSuccessResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Get(
+ *     path="/api/v1/file/{uuid}",
+ *     summary="Find File",
+ *     tags={"File"},
+ *    @OA\Parameter(
+ *          name="uuid",
+ *          in="path",
+ *          description="file uuid",
+ *          required=true,
+ *          @OA\Schema(type="string")
+ *    ),
+ *     @OA\Response(response=200, description="OK", @OA\JsonContent()),
+ *     @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+ *     @OA\Response(response=422, description="Unprocessable Entity", @OA\JsonContent()),
+ *     @OA\Response(response=404, description="Page not found", @OA\JsonContent()),
+ *     @OA\Response(response=500, description="Internal server error", @OA\JsonContent())
+ * )
+ */
 class GetFileController extends Controller
 {
     private FileBLLInterface $fileBLL;
--- laravel-petshop/app/Domain/File/Controllers/File/UploadFileController.php
+++ laravel-petshop/app/Domain/File/Controllers/File/UploadFileController.php
@@ -6,7 +6,34 @@ use App\Domain\File\BLL\File\FileBLLInterface;
 use App\Domain\File\Requests\UploadFileRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\ApiSuccessResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Post(
+ *     path="/api/v1/file/upload",
+ *     summary="Upload file",
+ *     tags={"File"},
+ *     security={{ "bearerAuth": {} }},
+ *     @OA\RequestBody(
+ *          @OA\MediaType(
+ *              mediaType="multipart/form-data",
+ *              @OA\Schema(
+ *                  @OA\Property(
+ *                      property="file",
+ *                      type="file",
+ *                      format="file"
+ *                  ),
+ *                  required={"file"}
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response=200, description="OK", @OA\JsonContent()),
+ *     @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+ *     @OA\Response(response=422, description="Unprocessable Entity", @OA\JsonContent()),
+ *     @OA\Response(response=404, description="Page not found", @OA\JsonContent()),
+ *     @OA\Response(response=500, description="Internal server error", @OA\JsonContent())
+ * )
+ */
 class UploadFileController extends Controller
 {
     private FileBLLInterface $fileBLL;
